9fdcc28ec41ffcaa408b1e934ca3bc569b4880ef
Tim <tim@evolvewebdesigns.com>
2023-05-26 19:23:30 -0700

    added modals for stats

--- src/App2.js
+++ src/App2.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import PlayerModal from './components/child';
-import { Button } from 'react-bootstrap';
-import { useState } from "react";
-import './App.css'
+// App.js
+import React, { useState } from 'react';
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
 import players from './players.json'
+// import PieChart from "./components/PieChart";
+import "./App.css";
+import "./bootstrap.min.css"
+// import {data} from "../utils/Data";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-export default function App() {
-    const [data, setData] = useState([]);
-    const [show, setShow] = useState('');
-    const handleShow = () => {
-        setShow(true);
-    }
-    const handleData = () => {
-        setData(JSON.stringify(players));
-    }
+Chart.register(CategoryScale);
+
+export function PlayerDetails() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
-        <div>
-            <PlayerModal handleShow={show} handleData={data}/>
-            <Button primary onClick={() => {
-                handleShow();
-                handleData();
-                }}>Click Parent</Button>
-        </div>
+        <>
+            <div className={"d-flex flex-wrap justify-content-center"}>
+
+                {players?.map((players) => (
+
+                    <Card className={"m-2"} style={{ width: '18rem' }}>
+                        <Card.Img variant={"top"} src={players.imagesrc} />
+                        <Card.Body>
+                            <Card.Title>{players.name}</Card.Title>
+                            <Card.Text>Jersey #: {players.jersey}</Card.Text>
+                            <Button variant={"primary"} onClick={handleShow}>See Stats</Button>
+                        </Card.Body>
+                    </Card>
+                ))}
+                {players?.map((players) => (
+                    <Modal show={show} onHide={handleClose} dialogClassName={"modal-90w"} aria-labelledby="example-custom-modal-styling-title">
+                        <Modal.Header closeButton>
+                            <Modal.Title id="example-custom-modal-styling-title">Modal heading</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Card className={"m-2"} style={{ width: '18rem' }}>
+                                <Card.Img variant={"top"} src={players.imagesrc} />
+                                <Card.Body>
+                                    <Card.Title>{players.name}</Card.Title>
+                                    <Card.Text>{players.id}</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="primary" onClick={handleClose}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                ))}
+            </div>
+        </>
     )
 }
+
+export function NewCards() {
+
+
+    return (
+        <Card className={"m-2"} style={{ width: '18rem' }}>
+            <Card.Img variant={"top"} src={"./logo512.png"} />
+            <Card.Body>
+                <Card.Title>Hello</Card.Title>
+                <Card.Text>Here is some text</Card.Text>
+                <Button variant={"primary"}>See Stats</Button>
+            </Card.Body>
+        </Card>
+    )
+}
+export default function App() {
+
+    return (
+        <>
+            <PlayerDetails />
+        </>
+    );
+}
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './AppNoModal';
+import App from './AppModal';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
